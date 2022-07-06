41c417f891f21a55bebb2b78a8117cd2f5753a91
mohammed-dunzo <104075408+mohammed-dunzo@users.noreply.github.com>
2022-07-06 19:05:24 +0530

    updated styling

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,22 +13,29 @@ public class Main {
         try (BufferedReader br = new BufferedReader(new FileReader(PATH))) {
             String line;
             String[] strings = new String[2];
+            System.out.println("**************************************  X  *********************************************");
+            System.out.println();
+            System.out.println();
             while ((line = br.readLine()) != null) {
                 String newLine = line.trim();
                 if (newLine.contains("_")){
                     strings =newLine.split(" ");
-                    System.out.println(ConsoleColors.BLUE+"@SerializedName(\""+strings[2].replaceAll(";", "")+"\")");
+                    System.out.println(ConsoleColors.YELLOW+"@SerializedName(\""+strings[2].replaceAll(";", "")+"\")"
+                    +ConsoleColors.RESET);
                     strings[2] = CaseFormat.UPPER_UNDERSCORE.to(CaseFormat.LOWER_CAMEL, strings[2]);
-                    System.out.println(ConsoleColors.BLUE + strings[0]+" "+strings[1]+" "+strings[2] +
+                    System.out.println(ConsoleColors.PURPLE + strings[0]+" "+strings[1]+" "+strings[2] +
                             ConsoleColors.RESET);
                 }else {
-                    System.out.println(ConsoleColors.YELLOW + newLine +
+                    System.out.println(ConsoleColors.PURPLE + newLine +
                             ConsoleColors.RESET);
                 }
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+        System.out.println();
+        System.out.println();
+        System.out.println("**************************************  X  *********************************************");
 
     }
 }
