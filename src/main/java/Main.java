ba862ae6f2226bb29e8244cdf28482e5c840f669
mohammed-dunzo <104075408+mohammed-dunzo@users.noreply.github.com>
2022-07-06 18:49:58 +0530

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,34 @@
+import com.google.common.base.CaseFormat;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main {
+
+    public static final String PATH = "/Users/mohammed/camel_case_convertor/input.txt";
+
+    public static void main(String[] args) throws FileNotFoundException {
+        try (BufferedReader br = new BufferedReader(new FileReader(PATH))) {
+            String line;
+            String[] strings = new String[2];
+            while ((line = br.readLine()) != null) {
+                String newLine = line.trim();
+                if (newLine.contains("_")){
+                    strings =newLine.split(" ");
+                    System.out.println(ConsoleColors.BLUE+"@SerializedName(\""+strings[2].replaceAll(";", "")+"\")");
+                    strings[2] = CaseFormat.UPPER_UNDERSCORE.to(CaseFormat.LOWER_CAMEL, strings[2]);
+                    System.out.println(ConsoleColors.BLUE + strings[0]+" "+strings[1]+" "+strings[2] +
+                            ConsoleColors.RESET);
+                }else {
+                    System.out.println(ConsoleColors.YELLOW + newLine +
+                            ConsoleColors.RESET);
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
